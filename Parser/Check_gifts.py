4c2ccb9ed0a8bb918c2f689fd536fac422d5728c
Xpommoc <rastafaster99@gmail.com>
2020-06-20 18:15:05 +0300

    gifts calc images

---
+++ Parser/Check_gifts.py
@@ -0,0 +1,75 @@
+import requests
+import json
+import parser_urls
+import os
+
+base_image_url = 'https://st.mafiaonline.ru/images/gifts/'
+json_load_url = 'https://www.mafiaonline.ru/api/api.php?action=gifts&param=gifts'
+
+
+def save_image(base_url, path, filename, file_extention):
+    url = base_url + filename + file_extention
+    r = requests.get(url)
+    if r.status_code == 200:
+        print('Картинка загружена, сохраняем... ')
+        if not os.path.exists(path):
+            print('path ' + path + ' doesnt exist. Creating new one...')
+            os.makedirs(path)
+        with open(path + filename + file_extention, "wb") as f:
+            f.write(r.content)
+            print('Сохранение прошло успешно!')
+    else:
+        print('Не удалось загрузить картинку по ссылке ' + url)
+
+def read_all_gifts(path):
+    with open(path, 'r', encoding='utf8') as f:
+        return json.load(f)
+
+def write_to_file(json_file, path):
+    with open(path, 'w', encoding='utf8') as f:
+        json.dump(json_file, f, indent=4, separators=(',', ': '), ensure_ascii=False)
+
+def load_json_from_ulr(url):
+    r = requests.get(url)
+    return r.json()
+
+def check_update():
+    json_cached = read_all_gifts(parser_urls.GIFTS_ALL_JSON)
+    json_loaded = load_json_from_ulr(json_load_url)
+    num_cached = len(json_cached['gifts'])
+    num_loaded = len(json_loaded['gifts'])
+    print(' К-во картинок в кешированном json : ' + str(num_cached))
+    print(' К-во картинок в мафском json : ' + str(num_loaded))
+    if num_loaded > num_cached:
+        new_images = num_loaded - num_cached
+        print('Есть новые картинки. К-во : ' + str(new_images) + ' шт.')
+        start_index = num_loaded - new_images
+        filetype = '.' + json_loaded['filetype']
+        for id in range(start_index, num_loaded):
+            print(' Загружаю картинку № ' + str(id))
+            img_section = json_loaded['gifts'][id]['section']
+            img_name = str(json_loaded['gifts'][id]['filename'])
+            save_path = parser_urls.GIFTS_IMAGE_LOCATION + str(img_section) + '/'
+            print('full save path = ' + save_path + img_name + filetype)
+            save_image(base_image_url, save_path, img_name, filetype)
+        write_to_file(json_loaded, parser_urls.GIFTS_ALL_JSON)
+        print('Загруженный json сохранен!')
+    else:
+        print('Новых картинок нет!')
+
+def load_all_images():
+    json_cached = read_all_gifts(parser_urls.GIFTS_ALL_JSON)
+    num_loaded = len(json_cached['gifts'])
+    filetype = '.' + json_cached['filetype']
+    for id in range(0, num_loaded):
+        print(' Загружаю картинку № ' + str(id))
+        img_section = json_cached['gifts'][id]['section']
+        img_name = str(json_cached['gifts'][id]['filename'])
+        save_path = parser_urls.GIFTS_IMAGE_LOCATION + str(img_section) + '/'
+        print('full save path = ' + save_path + img_name + filetype)
+        save_image(base_image_url, save_path, img_name, filetype)
+    print('Сохранение прошло успешно.')
+
+load_all_images()
+
+#check_update()
--- Parser/parser_urls.py
+++ Parser/parser_urls.py
@@ -2,6 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+MEDIAFILES_DIRS = [os.path.join(BASE_DIR, "media")]
 
 OFFICIAL_TOURNAMENT_URL = 'https://www.mafiaonline.ru/forum/viewforum.php?id=53'
 NONOFFICIAL_TOURNAMENT_URL = 'https://www.mafiaonline.ru/forum/viewforum.php?id=58'
@@ -15,3 +16,8 @@ MAF_STAT_URL = 'https://mafiastat.ru/clan/view/25440'
 
 STAT_JSON_PATH = STATICFILES_DIRS[0] + '/cache/mafstat.json'
 FORUM_JSON_PATH = STATICFILES_DIRS[0] + '/cache/forum.json'
+
+GIFTS_ALL_JSON = STATICFILES_DIRS[0] + '/json/gifts_all.json'
+GIFTS_CATEGORIES_JSON = STATICFILES_DIRS[0] + '/json/gifts_categories.json'
+
+GIFTS_IMAGE_LOCATION = MEDIAFILES_DIRS[0] + '/Gift_Images/'
